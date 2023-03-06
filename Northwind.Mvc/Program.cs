1cddd7edc4de4d311844410b65b23208cf6451b1
Luis D´Andrea <87934926+Luisinccs@users.noreply.github.com>
2023-03-06 17:05:38 -0400

    MVC part 1

---
+++ Northwind.Mvc/Program.cs
@@ -0,0 +1,47 @@
+﻿// Section 1 - Impotrs namespaces
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Mvc.Data;
+using Packt.Shared;
+
+// Section 2 - Configures the host web server including services
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Registers an appliction database context using SQLite. The database connection string is loaded from the appsettings.json file.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// adds ASP.NET Core Identifynfor authentication and configures it to use the application database
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+// Adds support for MVC controllers with views
+builder.Services.AddControllersWithViews();
+builder.Services.AddNorthwindContext();
+var app = builder.Build();
+
+// Section 3 Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseMigrationsEndPoint();
+} else {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// Section 4 - start the host web server listening for HTTP requests
+app.Run();
+
