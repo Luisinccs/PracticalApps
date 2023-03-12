2b432fe65da910959eb4212859f4602b8805dcee
Luis D´Andrea <87934926+Luisinccs@users.noreply.github.com>
2023-03-12 17:50:42 -0400

    Blazor sin terminar
    Algunos problemas

---
+++ Northwind.BlazorServer/Data/INorthwindService.cs
@@ -0,0 +1,12 @@
+﻿namespace Packt.Shared;
+
+public interface INorthwindService {
+
+    Task<List<Customer>> GetCustomersAsync();
+    Task<List<Customer>> GetCustomersAsync(string country);
+    Task<Customer?> GetCustomerAsync(string id);
+    Task<Customer> CreateCustomerAsync(Customer customer);
+    Task<Customer> UpdateCustomerAsync(Customer customer);
+    Task DeleteCustomerAsync (string id);
+
+}
---
+++ Northwind.BlazorServer/Data/NorthwindService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared;
+
+public class NorthwindService : INorthwindService {
+
+    private readonly NorthwindContext db;
+
+    public NorthwindService(NorthwindContext db) {
+        this.db = db;
+    }
+
+
+    public Task<Customer> CreateCustomerAsync(Customer customer) {
+        db.Customers.Add(customer);
+        db.SaveChangesAsync();
+        return Task.FromResult(customer);
+    }
+
+    public Task DeleteCustomerAsync(string id) {
+
+        Customer? customer = db.Customers.FirstOrDefaultAsync
+        (c => c.CustomerId == id).Result;
+        if (customer == null) {
+            return Task.CompletedTask;
+        }
+        else {
+            db.Customers.Remove(customer);
+            return db.SaveChangesAsync();
+        }
+
+    }
+
+    public Task<Customer?> GetCustomerAsync(string id) =>
+        db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+
+    public Task<List<Customer>> GetCustomersAsync() => db.Customers.ToListAsync();
+
+    public Task<List<Customer>> GetCustomersAsync(string country) =>
+        db.Customers.Where(c => c.Country == country).ToListAsync();
+
+    public Task<Customer> UpdateCustomerAsync(Customer customer) {
+        db.Entry(customer).State = EntityState.Modified;
+        db.SaveChangesAsync();
+        return Task.FromResult(customer);
+    }
+}
